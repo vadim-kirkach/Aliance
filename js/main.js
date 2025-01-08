fc5f31b54535618cfb17caffe3a7a6ee37b8ab35
vadim-kirkach <kirkach.digital@gmail.com>
2025-01-08 15:29:36 +0300

    fix: swiper slider

--- js/main.js
+++ js/main.js
@@ -13,39 +13,54 @@ const lightModeOff = (event) => {
   logo.href.baseVal = "img/sprite.svg#logo-light";
 }
 
-const openMenu = (event) => { //функция открывания меню
-  menu.classList.add("is-open"); //вешает класс из-опен
+const openMenu = (event) => { // функция открывания меню
+  menu.classList.add("is-open"); // вешает класс из-опен
   mMenuToggle.classList.add("close-menu");
-  document.body.style.overflow="hidden"; //запрещаем прокрутку сайта под меню
+  document.body.style.overflow="hidden"; // запрещаем прокрутку сайта под меню
   lightModeOn();
 };
 
-const closeMenu = (event) => { //функция закрывания меню
-  menu.classList.remove("is-open"); //убирает класс из-опен
+const closeMenu = (event) => { // функция закрывания меню
+  menu.classList.remove("is-open"); // убирает класс из-опен
   mMenuToggle.classList.remove("close-menu");
-  document.body.style.overflow=""; //возвращаем прокрутку сайта под меню
+  document.body.style.overflow=""; // возвращаем прокрутку сайта под меню
   lightModeOff();
 };
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
   menu.classList.contains("is-open") ? closeMenu() : openMenu();
-  });
+});
 
-  const swiper = new Swiper('.swiper', {
-    speed: 400,
-    autoHeight: true,
-    loop: true,
-    spaceBetween: 100,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+    
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
     },
-  });
-  
-
-  
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  } 
+});
