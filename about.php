d276eaf60f930728833ba2a9c2c6a8f483d469a3
vadim-kirkach <kirkach.digital@gmail.com>
2025-01-25 14:23:52 +0300

    add: php pages and script

---
+++ about.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wdth,wght@0,62.5..100,100..900;1,62.5..100,100..900&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="/css/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="/css/normalize.css"/>
+    <link rel="stylesheet" href="/css/style.css"/>
+    <title>О компании - Aliance Production</title>
+  </head>
+<body>
+  <div class="mobile-menu">
+    <ul class="mobile-menu-nav">
+      <li class="mobile-menu-nav-item">
+        <a href="#" class="mobile-menu-link">О компании</a>
+      </li>
+
+      <li class="mobile-menu-nav-item">
+        <a href="#" class="mobile-menu-link">Контрактное производство</a>
+        <ul class="mobile-submenu">
+          <li class="mobile-submenu-item">
+            <a class="mobile-submenu-link" href="#">Автомобильная химия</a>
+          </li>
+          <li class="mobile-submenu-item">
+            <a class="mobile-submenu-link" href="#">Бытовая химия</a>
+          </li>
+          <li class="mobile-submenu-item">
+            <a class="mobile-submenu-link" href="#">Дезинфицирующие средства</a>
+          </li>
+          <li class="mobile-submenu-item">
+            <a class="mobile-submenu-link" href="#">Пищевые аэрозоли</a>
+          </li>
+          <li class="mobile-submenu-item">
+            <a class="mobile-submenu-link" href="#">Косметическая продукция</a>
+          </li>
+          <li class="mobile-submenu-item">
+            <a class="mobile-submenu-link" href="#">Краски аэрозольные</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="mobile-menu-nav-item">
+        <a href="#" class="mobile-menu-link">Собственные торговые марки</a>
+      </li>
+
+      <li class="mobile-menu-nav-item">
+        <a href="#" class="mobile-menu-link">Новости</a>
+      </li>
+
+      <li class="mobile-menu-nav-item">
+        <a href="#" class="mobile-menu-link">Контакты</a>
+      </li>
+    </ul>
+    <a href="tel:+74996861014" class="mobile-phone">+7 (499) 686-10-14</a>
+    <div class="mobile-info">
+      <svg class="mobile-icon" width="24px" height="24px">
+        <use href="img/sprite.svg#mark"></use>
+      </svg>
+      <address class="mobile-info-address">г. Москва, Холодильный пер. 4к1с8</address>
+    </div>
+
+    <div class="mobile-info">
+      <svg class="mobile-icon" width="24px" height="24px">
+        <use href="img/sprite.svg#mail"></use>
+      </svg>
+      <a href="mailto:a.dragunov@tdaliance.ru" class="mobile-info-email">a.dragunov@tdaliance.ru</a>
+    </div>
+    <div class="mobile-info">
+
+    <a href="#" class="mobile-info-email">
+        <svg class="mobile-icon" width="24px" height="24px">
+        <use href="img/sprite.svg#vk"></use>
+      </svg>
+    </a>
+    <a href="#" class="mobile-info-email">
+        <svg class="mobile-icon" width="24px" height="24px">
+        <use href="img/sprite.svg#inst"></use>
+      </svg>
+    </a>
+
+    </div>
+
+  </div>
+
+  <nav class="navbar navbar-light">
+    <a href="" class="mobile-menu-toggle">
+      <div class="mobile-menu-line"></div>
+      <div class="mobile-menu-line"></div>
+      <div class="mobile-menu-line"></div>
+    </a>
+    <a href="./" class="header-logo">
+      <svg class="logo-svg logo-light" width="140px" height="44px">
+        <use href="img/sprite.svg#logo-light"></use>
+      </svg>
+      <svg class="logo-svg logo-dark" width="140px" height="44px">
+        <use href="img/sprite.svg#logo"></use>
+      </svg>
+    </a>
+    <ul class="header-nav">
+      <li class="header-nav-item">
+        <a href="#" class="header-nav-link">О компании</a>
+      </li>
+
+      <li class="header-nav-item">
+        <a href="#" class="header-nav-link">Контрактное производство</a>
+      </li>
+
+      <li class="header-nav-item">
+        <a href="#" class="header-nav-link">Собственные торговые марки</a>
+      </li>
+
+      <li class="header-nav-item">
+        <a href="#" class="header-nav-link">Новости</a>
+      </li>
+
+      <li class="header-nav-item">
+        <a href="#" class="header-nav-link">Контакты</a>
+      </li>
+    </ul>
+
+    <div class="header-phone">
+      <svg class="phone-icon" width="24px" height="24px">
+        <use href="img/sprite.svg#phone"></use>
+      </svg>
+      <a href="tel:+74996861014" class="header-phone-link">+7 (499) 686-10-14</a>
+    </div>
+    <!-- /.header-phone -->
+     <button class="navbar-button button" data-toggle="modal">
+      <svg class="button-icon" width="24px" height="24px">
+        <use href="img/sprite.svg#phone"></use>
+      </svg>
+      <span class="button-text">Получить консультацию</span>
+     </button>
+
+  </nav>
+  <!-- /.navbar -->
+ 
+  <section class="section header-grey">
+    <div class="container">
+      <div class="header-grey-wrapper">
+        <div class="seporator"></div>
+      <h1 class="header-grey-title">О компании</h1>
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link" href="# ">Главная</a></li>
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link" href="#">О компании</a></li>
+       </ul>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="section header-grey">
+    <img src="/img/avto-him.png" alt="" class="header-grey-img">
+    <div class="container">
+      <div class="header-grey-wrapper">
+        <div class="seporator"></div>
+      <h1 class="header-grey-title">Автомобильная химия</h1>
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link" href="# ">Главная</a></li>
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link" href="#">Контрактное производство</a></li>
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link" href="#">Автомобильная химия</a></li>
+      </ul>
+       
+      </div>
+    </div>
+  </section>
+
+  <section class="section header-picture">
+    <div class="container">
+      <div class="header-grey-wrapper">
+        <div class="seporator"></div>
+      <h1 class="header-grey-title header-grey-title-white">Современная методология разработки одухотворила всех причастных</h1>
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link breadcrumbs-link-white" href="# ">Главная</a></li>
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link breadcrumbs-link-white" href="#">Блог</a></li>
+        <li class="breadcrumbs-li"><a class="breadcrumbs-link breadcrumbs-link-white" href="#">Современная методология разработки одухотворила всех причастных</a></li>
+      </ul>
+       
+      </div>
+    </div>
+  </section>
+
+
+  <?php include_once('footer.php');?>
---
+++ handler.php
@@ -0,0 +1,6 @@
+<?php 
+
+$user_name = $_POST["username"];
+$user_phone = $_POST["userphone"];
+echo "Привет, " . $user_name . "<br>";
+echo "Ваш телефон: <b>" . $user_phone . "</b>";
