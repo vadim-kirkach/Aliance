6a4758cb4336225c61551d58e2773780ed757d05
vadim-kirkach <kirkach.digital@gmail.com>
2025-02-17 14:16:06 +0300

    add: content blog and privacy

--- blog.php
+++ blog.php
@@ -2,4 +2,129 @@
 $page_title = 'Блог';
 $header_style = 'header-grey header-grey-img';
 include_once('header-page.php');?>
+
+<section class="section blog">
+    <div class="container">
+    <div class=" blog-cards-page">
+  <!-- Additional required wrapper -->
+  
+    <!-- Slides -->
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog1.webp">
+              <source type="image/png" srcset="/img/blog/blog1.png">
+              <img class="blog-card-image" src="/img/blog/blog1.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Современная методология разработки одухотворила всех причастных</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog2.webp">
+              <source type="image/png" srcset="/img/blog/blog2.png">
+              <img class="blog-card-image" src="/img/blog/blog2.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Сложно сказать, почему жизнь прекрасна</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog1.webp">
+              <source type="image/png" srcset="/img/blog/blog1.png">
+              <img class="blog-card-image" src="/img/blog/blog1.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Современная методология разработки одухотворила всех причастных</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog2.webp">
+              <source type="image/png" srcset="/img/blog/blog2.png">
+              <img class="blog-card-image" src="/img/blog/blog2.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Сложно сказать, почему жизнь прекрасна</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog1.webp">
+              <source type="image/png" srcset="/img/blog/blog1.png">
+              <img class="blog-card-image" src="/img/blog/blog1.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Современная методология разработки одухотворила всех причастных</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog2.webp">
+              <source type="image/png" srcset="/img/blog/blog2.png">
+              <img class="blog-card-image" src="/img/blog/blog2.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Сложно сказать, почему жизнь прекрасна</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog1.webp">
+              <source type="image/png" srcset="/img/blog/blog1.png">
+              <img class="blog-card-image" src="/img/blog/blog1.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Современная методология разработки одухотворила всех причастных</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog2.webp">
+              <source type="image/png" srcset="/img/blog/blog2.png">
+              <img class="blog-card-image" src="/img/blog/blog2.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Сложно сказать, почему жизнь прекрасна</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog1.webp">
+              <source type="image/png" srcset="/img/blog/blog1.png">
+              <img class="blog-card-image" src="/img/blog/blog1.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Современная методология разработки одухотворила всех причастных</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+    <a href="#" class=" blog-card">
+        <picture class="blog-card-image">
+              <source type="image/webp" srcset="/img/blog/blog2.webp">
+              <source type="image/png" srcset="/img/blog/blog2.png">
+              <img class="blog-card-image" src="/img/blog/blog2.png" alt="">
+        </picture>
+      
+      <h3 class="blog-card-title">Сложно сказать, почему жизнь прекрасна</h3>
+      <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую ситуацию, в равной степени предоставлены...</p>
+    </a>
+    </div>
+    <div class="num-list-wrapper">
+    <ol class="num-list">
+    <li class="num-list-item active-page"><a class="num-link" href="#">1</a></li>
+    <li class="num-list-item"><a class="num-link" href="#">2</a></li>
+    <li class="num-list-item"><a class="num-link" href="#">3</a></li>
+    <li class="num-list-item"><a class="num-link" href="#">4</a></li>
+    <li class="num-list-item"><a class="num-link" href="#">5</a></li>
+    </ol>
+    </div>
+
+    </div>
+</section>
+
 <?php include_once('footer.php');?>
