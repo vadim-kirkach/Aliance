e8247751c11d610f64556e14e9bce0ffd38b34b6
vadim-kirkach <kirkach.digital@gmail.com>
2025-02-17 20:27:38 +0300

    fix: pixel perfect

--- template-parts/header-grey-img.php
+++ template-parts/header-grey-img.php
@@ -1,1 +1,5 @@
-<img src="/img/avto-him.png" alt="" class="header-grey-img">
+<picture class="header-grey-img">
+            <source type="image/webp" srcset="/img/avto-him.webp">
+            <source type="image/jpeg" srcset="/img/avto-him.png">
+            <img class="header-grey-img" src="/img/avto-him.png" alt="">
+</picture>
